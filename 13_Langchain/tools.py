c3ad8d9e59defbfc36de105e8cc0da1e18101243
Kim Sungwhan <kgmyh@naver.com>
2025-06-20 11:44:24 +0900

    up

--- 13_Langchain/tools.py
+++ 13_Langchain/tools.py
@@ -4,6 +4,9 @@ from langchain_tavily import TavilySearch
 from langchain_core.tools import tool
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_core.runnables import chain
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
 from pydantic import BaseModel, Field
 
 @tool
@@ -58,3 +61,38 @@ search_wiki = wikipedia_search.as_tool(
                  "일반적인 지식이나 배경 정보가 필요한 경우 유용하게 사용할 수있는 tool입니다."),
     args_schema=SearchWikiArgsSchema # 파라미터(argument)에대한 설계 -> pydantic 모델 정의
 )
+
+
+#######################
+# 메뉴 검색 툴
+########################
+COLLECTION = "restaurant_menu"
+PERSIST_DIRECTORY = "vector_store/chroma/restaurant_menu_db"
+
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+vector_store = Chroma(
+    embedding_function=embedding_model,
+    collection_name=COLLECTION,
+    persist_directory=PERSIST_DIRECTORY
+)
+
+# Retriever 생성
+menu_retriever = vector_store.as_retriever(
+    search_kwargs={"k":3}
+)
+
+@tool
+def search_menu(query:str) -> dict:
+    """Vector Store(Vector Database)에 저장된 레스토랑의 메뉴를 검색하는 tool.
+    이 tool은 query와 가장 연관된 메뉴를 Vector Database에서 검색해서 반환한다.
+    레스토랑 메뉴와 관련된 검색은 이 tool을 사용한다.
+    """
+    menu_result_list = menu_retriever.invoke(query)
+    result_list = []
+    for menu_doc in menu_result_list:
+        result_list.append({"content":menu_doc.page_content, 
+                            "title":menu_doc.metadata['menu_name'], 
+                            "url":menu_doc.metadata["source"]})
+    if not result_list: # 비었으면 -> 검색결과가 없으면
+        result_list = "검색된 정보가 없습니다."
+    return {"result": result_list}
